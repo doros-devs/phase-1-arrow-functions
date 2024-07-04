72e70253caeb87303c85eee647279c483688b1c7
terenceouma <doros.devs@gmail.com>
2024-07-04 12:31:37 +0300

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(){
+    return 2000 / 100
+}
+
+divide();
+
+const square = parameter1 => (parameter1 * parameter1);
+square();
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add();
